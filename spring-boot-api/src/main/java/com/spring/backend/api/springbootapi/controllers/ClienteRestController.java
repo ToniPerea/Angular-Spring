11ae27374eeaaed13fb977595d4c569a393f1286
ToniPerea <apepa31@gmail.com>
2023-05-07 19:47:51 +0200

    Controlar errores en Spring

--- spring-boot-api/src/main/java/com/spring/backend/api/springbootapi/controllers/ClienteRestController.java
+++ spring-boot-api/src/main/java/com/spring/backend/api/springbootapi/controllers/ClienteRestController.java
@@ -2,12 +2,17 @@ package com.spring.backend.api.springbootapi.controllers;
 
 import com.spring.backend.api.springbootapi.models.entity.Cliente;
 import com.spring.backend.api.springbootapi.models.services.IClienteService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -24,32 +29,93 @@ public class ClienteRestController {
 
     @GetMapping("/clientes/{id}")
     //@ResponseStatus(HttpStatus.OK) es redundante porque ya lo indican los metodos internos ya que un ok es un 200
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id){
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            cliente = clienteService.findById(id);
+        } catch(DataAccessException e){
+            response.put("mensaje", "Error al realizar la consulta!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(cliente == null){
+            response.put("mensaje", "El cliente ID:".concat(id.toString().concat(" no existe en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public ResponseEntity<?> create(@RequestBody Cliente cliente){
         //cliente.setCreateAt(new Date()); Como tenemos en el model el decorador PrePersist, alli hacemos esto
-        return clienteService.save(cliente);
+
+        Cliente newCliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            newCliente = clienteService.save(cliente);
+        }catch(DataAccessException e){
+            response.put("mensaje", "Error al realizar la insercion!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El cliente ha sido creado con existo!");
+        response.put("cliente", newCliente);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/clientes/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id){
+
         Cliente clienteActual = clienteService.findById(id);
+        Cliente clienteActualizado = null;
+        Map<String, Object> response = new HashMap<>();
 
-        clienteActual.setApellido(cliente.getApellido());
-        clienteActual.setNombre(cliente.getNombre());
-        clienteActual.setEmail(cliente.getEmail());
+        if(clienteActual == null){
+            response.put("mensaje", "Error, no se puedo editar el cliente ID:".concat(id.toString().concat(" no existe en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
 
-        return clienteService.save(clienteActual);
+        try{
+            clienteActual.setApellido(cliente.getApellido());
+            clienteActual.setNombre(cliente.getNombre());
+            clienteActual.setEmail(cliente.getEmail());
+            clienteActual.setCreateAt(cliente.getCreateAt());
+
+            clienteActualizado = clienteService.save(clienteActual);
+        }catch(DataAccessException e){
+            response.put("mensaje", "Error al actualizar el cliente!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El cliente ha sido actualizado con existo!");
+        response.put("cliente", clienteActualizado);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/clientes/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
-        clienteService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable Long id){
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            clienteService.delete(id);
+        }catch(DataAccessException e){
+            response.put("mensaje", "Error al eliminar el cliente!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El cliente ha sido eliminado con exito!");
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+
     }
 }
--- spring-boot-api/src/main/resources/import.sql
+++ spring-boot-api/src/main/resources/import.sql
@@ -1,22 +1,22 @@
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Antonio', 'Perea','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pepe', 'Pepon','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Javie', 'John','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Manuel', 'Loco','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Hola', 'Mundo','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Iphone', '11','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Samsung', 'S23','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Segovia','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Lopez', 'Guzman','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Marta', 'Madrid','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pepe', 'Agua','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Fer', 'Pan','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Juan', 'Parras','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ana', 'Alcudia','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Laura', 'Cordoba','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Maria', 'Sevilla','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ejemplo', 'Fernandez','nada@gmail.com','2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pedro', 'Locura','nada@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman','nada1@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Antonio', 'Perea','nada2@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pepe', 'Pepon','nada3@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Javie', 'John','nada4@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Manuel', 'Loco','nada5@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Hola', 'Mundo','nada6@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Iphone', '11','nada7@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Samsung', 'S23','nada8@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Segovia','nada9@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Lopez', 'Guzman','nada10@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Marta', 'Madrid','nada11@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pepe', 'Agua','nada12@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Fer', 'Pan','nada13@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Juan', 'Parras','nada14@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ana', 'Alcudia','nada15@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Laura', 'Cordoba','nada16@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Maria', 'Sevilla','nada17@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ejemplo', 'Fernandez','nada18@gmail.com','2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Pedro', 'Locura','nada19@gmail.com','2018-01-02');
 
 
 
